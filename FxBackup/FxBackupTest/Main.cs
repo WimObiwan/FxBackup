9e5e01e00cb6a5e040a9d966b17310d343ddb132
Wim Devos <wim@obiwan.be>
2012-06-14 11:00:50 +0200

    Added verification of restore

--- FxBackup/FxBackupTest/Main.cs
+++ FxBackup/FxBackupTest/Main.cs
@@ -1,11 +1,14 @@
 using System;
 using FxBackupLib;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FxBackupTest
 {
 	class MainClass
 	{
+		static string originPath = @"C:\Data\Portable Program Files";
 		static string archivePath = @"c:\temp\fxbtest";
 		static string restorePath = @"c:\temp\fxbtestrest";
 
@@ -16,21 +19,22 @@ namespace FxBackupTest
 
 		static void Run ()
 		{
-			Directory.Delete (archivePath, true);
+			/*Directory.Delete (archivePath, true);
 			Directory.Delete (restorePath, true);
 			Directory.CreateDirectory (restorePath);
 			Backup ();
 			Verify (VerifyEngine.VerificationType.ArchiveHashWithArchiveData);
 			Verify (VerifyEngine.VerificationType.ArchiveHashWithOriginData);
 			Verify (VerifyEngine.VerificationType.ArchiveDataWithOriginData);
-			Restore ();
+			Restore ();*/
+			TestRestore ();
 		}
 		
 		static void Backup ()
 		{
 			Archive archive = new Archive (new DirectoryStore (archivePath));
 			BackupEngine engine = new BackupEngine (archive);
-			engine.Origins.Add (new FileSystemOrigin (@"C:\Data\Portable Program Files"));
+			engine.Origins.Add (new FileSystemOrigin (originPath));
 			engine.Progress += delegate(object sender, OriginProgressEventArgs arg) {
 				switch (arg.State) {
 				case State.BeginItem:
@@ -68,7 +72,10 @@ namespace FxBackupTest
 		static void Restore ()
 		{
 			Archive archive = new Archive (new DirectoryStore (archivePath));
-			RestoreEngine engine = new RestoreEngine (new FileSystemOrigin (restorePath), archive);
+			RestoreEngine engine = new RestoreEngine (
+				new FileSystemOrigin (restorePath),
+				archive
+			);
 			engine.Progress += delegate(object sender, OriginProgressEventArgs arg) {
 				switch (arg.State) {
 				case State.BeginItem:
@@ -88,6 +95,76 @@ namespace FxBackupTest
 				}
 			};
 			engine.Run ();
+		}
+
+		static void TestRestore ()
+		{
+			if (TestRecursive (originPath, Path.Combine(restorePath, Path.GetFileName (originPath))))
+				Console.WriteLine ("Compare restore succeeded");
+			else
+				Console.WriteLine ("Compare restore failed");
+		}
+
+		static bool TestRecursive (string originPath, string restorePath)
+		{
+//			Console.WriteLine ("O {0}", originPath);
+//			Console.WriteLine ("R {0}", restorePath);
+			
+			bool same = true;
+			
+			IEnumerator<FileSystemInfo> origins = (new DirectoryInfo (originPath)).EnumerateFileSystemInfos ()
+				.OrderBy (p => p.Name)
+				.GetEnumerator ();
+			IEnumerator<FileSystemInfo> restores = (new DirectoryInfo (restorePath)).EnumerateFileSystemInfos ()
+				.OrderBy (p => p.Name)
+				.GetEnumerator ();
+			
+			FileSystemInfo origin, restore;
+			origin = origins.MoveNext () ? origins.Current : null;
+			restore = restores.MoveNext () ? restores.Current : null;
+			
+			while (origin != null || restore != null) {
+				int iCompare;
+				if (origin != null && restore != null)
+					iCompare = string.Compare (
+						origin.Name,
+						restore.Name,
+						StringComparison.InvariantCultureIgnoreCase);
+				else if (origin != null)
+					iCompare = -1;
+				else // if (restore != null)
+					iCompare = 1;
+								
+				if (iCompare < 0) {
+					same = false;
+					Console.WriteLine ("Item not in restore: {0}", origin.Name);
+					origin = origins.MoveNext () ? origins.Current : null;
+				} else if (iCompare > 0) {
+					same = false;
+					Console.WriteLine ("Item not in origin: {0}", restore.Name);
+					restore = restores.MoveNext () ? restores.Current : null;
+				} else if (iCompare == 0) {
+					if ((origin.Attributes & FileAttributes.Directory) != (restore.Attributes & FileAttributes.Directory)) {
+						Console.WriteLine ("Mismatch file/directory {0}", origin.FullName);
+					} else if ((origin.Attributes & FileAttributes.Directory) != 0) {
+						if (!TestRecursive (origin.FullName, restore.FullName))
+							same = false;
+					} else {
+						using (Stream input1 = new FileStream(origin.FullName, FileMode.Open, FileAccess.Read)) {
+							using (Stream input2 = new FileStream(restore.FullName, FileMode.Open, FileAccess.Read)) {
+								StreamVerifier streamVerifier = new StreamVerifier();
+								if (!streamVerifier.Verify (input1, input2))
+									same = false;
+							}
+						}
+					}
+					
+					origin = origins.MoveNext () ? origins.Current : null;
+					restore = restores.MoveNext () ? restores.Current : null;
+				}
+			}
+			
+			return same;
 		}
 	}
 }
